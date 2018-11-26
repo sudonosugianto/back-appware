b166a7079c78503d637f42b8a8be6bcee1258a44
Lutfi Zakaria <zakaria@alphatech.id>
2018-11-27 06:46:57 +0700

    Update Tabel

---
+++ modelActuaStock.py
@@ -0,0 +1,16 @@
+from models import db
+
+class ActualStock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # foreign key field
+    packageActualStocksID = db.Column(db.Integer, db.ForeignKey("packages.id", ondelete='CASCADE'), nullable=False)
+    # field
+    actual_stock = db.Column(db.Integer, default=0)
+    notes = db.Column(db.String(255))
+    status = db.Column(db.Boolean, default=1)
+    created_at = db.Column(db.DateTime, default= db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default= db.func.current_timestamp())
+
+    def __repr__(self):
+        return "<ActualStock %r>" % self.id
+
